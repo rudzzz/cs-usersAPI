03dd0d3970cb4158f18da3101a804527bc3cd254
rudzzz <rudsolza13@gmail.com>
2023-05-01 10:04:01 -0300

    models folder

---
+++ Models/UsersModel.cs
@@ -0,0 +1,9 @@
+﻿namespace WebAPI.Models
+{
+    public class UsersModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+}
